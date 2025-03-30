dee99483ed588cfa71132d9fba74a43d8bda5cfc
Daniel Bodky <dbodky@gmail.com>
2025-03-30 17:13:17 +0200

    feat: add opengraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,32 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Analytics } from '@vercel/analytics/next'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: 'Material for MkDocs Configurator',
-  description: 'Configure your documentation visually'
-}
+  title: "Material for MkDocs Configurator",
+  description: "Configure your documentation visually",
+  openGraph: {
+    title: "Material for MkDocs Configurator",
+    description: "Configure your documentation visually",
+    url: "https://materialconfig.dev",
+    siteName: "materialconfig.dev",
+    images: [
+      {
+        url: "https://materialconfig.dev/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "materialconfig.dev",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -19,5 +35,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
